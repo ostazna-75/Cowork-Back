b40c053d2ecb69bb281ada19ee45758145d23908
Islam <islam.elsharkawy@hotmail.com>
2019-09-19 20:50:13 +0200

    Comment // back end

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,17 +1,32 @@
+// userModel.js est comment sauvegarder les donnée du User dans le back
 /* ======= Dependencies ======= */
+// Toute les information que l'on aura dans user
+
 const mongoose = require("mongoose");
 
 // notre modèle (tjrs avec majuscule )user
 const User = new mongoose.model("User", {
-	email: String,
-	password: String,
-	token: String,
-	// hash et salt : 2 chaine de caratcere qui vont etre le resultat d'1 mixage entre le password, le uid2, le sha256 et 64
+  email: String,
+  password: String,
+  // token: String,
+  // token est ce que l'on envoie au front pour lui dire ( pour vérifier l'identité de user )
+  // permet de garder son email ouvert
+  // Le back va vérififer et aller dans labse de donnée et le mail qui corrspond c'est bon,
+  // le back dit que tu peut rester connecter
+  token: String,
+  // hash et salt : 2 chaine de caratcere qui vont etre le resultat d'1 mixage entre le password, le uid2, le sha256 et 64
+  // pour enregistrer le mot de pass en maniere crypté
+  // transformatiokn du password ou on ne peut pas revenir au password
+  hash: String,
+  salt: String,
+  // hash est le password tranformé par rapport au Salt
+  // quand on récupère le password, on le fait rtentré dand une fonction qui a le salt en argments
+  // et qui transfrome le tout en chaine de caractère qu'on va appelé Hash.
+  // quand l'utilisateur voudra se connecter et mettre son password , on utilisera mla meme fonction qui tranforme le password au salt
+  // et compare le password que l'user a rentrer avec le hash qui est dans la base de donnée ( qu'on a enregistré la premiere fois )
 
-	hash: String,
-	salt: String,
-	// dans son modèle il faudrait un account qui aura un username
-	username: String
+  // dans son modèle il faudrait un account qui aura un username
+  username: String
 });
 
 /* ======= Export the model ======= */
--- index.js
+++ index.js
@@ -1,30 +1,46 @@
+//index.js : c'est le point d'entrée sur le Serveur
+// Back = Serveur
+//------------------------------------->
+// npm install express
+// Le serveur express puisse tourner, pour l'utliser
+// express : sans express très compliqué de faire touner le back
+// ce sont des fonction qui permettetnt de faciliter l"écriture du back
+// Grace à express on peut faire des manipulation dans le backend
 const express = require("express");
 const app = express();
 
-// body parser sert a récupérer les information rentré danls es input par exemples
+// body parser sert a récupérer les information rentré dans es input par exemples
+// bodyparser sert à récupérer des informations que l'on envoie dans les requêtes
+// avec Postman logiciel qui permet de lire les infos dans le body
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
+// const cors =  pour pouvoir lire les fichier à partir d'un autre nom de domaine
 const cors = require("cors");
 app.use(cors());
 
-// base de donnée
+// MongoDb = base de donnée
+// On va pouvoir changer et manipuler les données qui sont envoyé par Postman ou les requête envoyé par postman ou l'application
 const mongoose = require("mongoose");
 mongoose.connect(
-	process.env.MONGODB_URI || "mongodb://localhost:27017/Coworking",
-	{
-		useNewUrlParser: true
-	}
+  process.env.MONGODB_URI || "mongodb://localhost:27017/Coworking",
+  {
+    useNewUrlParser: true
+  }
 );
-
+// Get sert à lire les donnée
 app.get("/", (req, res) => {
-	res.send("Bienvenue");
+  res.send("Bienvenue");
 });
 
 // Routes users
+// 1: pour récupérer toute les route dans le fichier user roads
+// 2 : Use est pour les utiliser
+
 const userRoads = require("./Routes/UserRoads");
 app.use(userRoads);
 
+// Permet de démarer le serveur, de l'écouter
 app.listen(process.env.PORT || 3000, () => {
-	console.log("Server Started");
+  console.log("Server Started");
 });
