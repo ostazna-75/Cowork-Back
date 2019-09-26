be99d35a3a44086632af76b1ee368795476a467e
Islam <islam.elsharkawy@hotmail.com>
2019-09-26 19:42:37 +0200

    deleat password and add token+hash+salt

--- Models/userModel.js
+++ Models/userModel.js
@@ -7,20 +7,21 @@ const mongoose = require("mongoose");
 // notre modèle (tjrs avec majuscule )user
 const User = new mongoose.model("User", {
   email: String,
-  password: String,
-  username: String
+  username: String,
+  token: String,
+  hash: String,
+  salt: String
   // token: String,
   // dans son modèle il faudrait un account qui aura un username
   // token est ce que l'on envoie au front pour lui dire ( pour vérifier l'identité de user )
   // permet de garder son email ouvert
-  // Le back va vérififer et aller dans labse de donnée et le mail qui corrspond c'est bon,
+  // Le back va vérififer et aller dans la base de donnée et le mail qui corrspond c'est bon,
   // le back dit que tu peut rester connecter
-  /*  token: String, */
+  /* token ----------------------------->  */
   // hash et salt : 2 chaine de caratcere qui vont etre le resultat d'1 mixage entre le password, le uid2, le sha256 et 64
   // pour enregistrer le mot de pass en maniere crypté
   // transformatiokn du password ou on ne peut pas revenir au password
-  /*   hash: String,
-  salt: String */
+  /* hash and salt-------------------------->  */
   // hash est le password tranformé par rapport au Salt
   // quand on récupère le password, on le fait rtentré dand une fonction qui a le salt en argments
   // et qui transfrome le tout en chaine de caractère qu'on va appelé Hash.
