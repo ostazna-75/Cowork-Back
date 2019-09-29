b24ac2c820e946502f05ac583b46c5b949a81103
Islam <islam.elsharkawy@hotmail.com>
2019-09-29 10:39:54 +0200

    Create Road for Cowork api
    Liste des titre de l'api dans fichier coworkmodel.js

---
+++ Models/coworkModel.js
@@ -0,0 +1,19 @@
+/* modèle : Les informations que nous aurontdans un cowork */
+
+const mongoose = require("mongoose");
+
+const Cowork = new mongoose.model("Cowork", {
+  brand: String /* Logo */,
+  bullet1: String /* surface entiere */,
+  bullet2: String /* nombre poste  */,
+  bullet3: String /* nombre salle */,
+  description: String /* descprition cowork */,
+  details: String /* details des lieu */,
+  image: String,
+  location: { lat: String, lon: String },
+  name: String,
+  street: String,
+  zipcode: String
+});
+
+module.exports = Cowork;
